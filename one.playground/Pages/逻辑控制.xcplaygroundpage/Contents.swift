98c7fd2b52cfe62a227886fc120f3b667ee2028c
vteso <vtesoho@163.com>
2019-10-07 20:03:22 +0800

    if

---
+++ one.playground/Pages/逻辑控制.xcplaygroundpage/Contents.swift
@@ -0,0 +1,39 @@
+//: [Previous](@previous)
+
+import Foundation
+
+let num = 20
+
+
+/*
+ 1 没有r括号
+ 2 必须有花括号
+ 3 没有非零即真，swift中只有 true / false
+ */
+if num > 10 {
+    print("大于10")
+}
+
+
+/*
+ if !_dataList {
+ 
+ }
+ if _dataList == nil{
+    
+ }
+*/
+
+// t条件必须要指明逻辑结果
+if num > 5 {
+    print("哈哈")
+}
+
+// 三目
+
+let a = 80
+let b = 20
+
+let c = a > b ? 100 : -100
+
+//: [Next](@next)
