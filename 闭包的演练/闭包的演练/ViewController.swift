213914bf5ee7d54570adac89481f07068c716661
vteso <vtesoho@163.com>
2019-10-25 21:12:34 +0800

    http

--- 闭包的演练/闭包的演练/ViewController.swift
+++ 闭包的演练/闭包的演练/ViewController.swift
@@ -15,7 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         
-        loadData()
+        var str = NSString(contentsOf: NSURL(string: "https://www.baidu.com")! as URL, encoding: String.Encoding.utf8.rawValue)
+        
+//        loadData()
         
     }
     
